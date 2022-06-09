e429afc11962edc7b53a6b715d4328a739fa7eae
Mo Hyungnan <hn.mo@brainz.co.kr>
2022-06-09 16:10:10 +0900

    초기 세팅

---
+++ my-app/src/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+import Head from './components/Head';
+
+function Main() {
+  return (
+    <div className="Main">
+      <Head />
+    </div>
+  );
+}
+
+export default Main;
